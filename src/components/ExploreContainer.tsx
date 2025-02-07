3d7b2934671824da01cb7afa5f292029cb6fe38e
lorbelle143 <lorbelleganzan@gmail.com>
2025-02-07 08:34:18 +0800

    hello

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,7 +5,10 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
+      <strong>welcome to stronghold?</strong>
+      <h1>i am chess warrior</h1>
+      <h2>you want to defeated</h2>
+      <h3>i kill you</h3>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
