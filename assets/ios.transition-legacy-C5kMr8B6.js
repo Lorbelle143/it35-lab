5efff9a2d194c0a28a47e2fc9eaa06a3dff3e6aa
lorbelle143 <lorbelleganzan@gmail.com>
2025-04-29 20:52:28 +0800

    Updates

--- assets/ios.transition-legacy-C5kMr8B6.js
+++ assets/ios.transition-legacy-C5kMr8B6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B3jh7uAo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CTMMe_6j.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
