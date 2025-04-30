b17cd269bc2975f90d290bb6b040228169397718
lorbelle143 <lorbelleganzan@gmail.com>
2025-04-30 18:01:47 +0800

    Updates

--- assets/md.transition-legacy-BToOK3q1.js
+++ assets/md.transition-legacy-BToOK3q1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CTMMe_6j.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-G9vfU9Uu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
