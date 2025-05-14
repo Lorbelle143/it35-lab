5dd1a9fb5a92ad9d3971000e638d16740623f2e2
ronabaldoza <20221238@nbsc.edu.ph>
2025-05-14 15:29:49 +0800

    Updates

--- assets/md.transition-legacy-C519UOvh.js
+++ assets/md.transition-legacy-C519UOvh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-G9vfU9Uu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-WY5wCrtC.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
