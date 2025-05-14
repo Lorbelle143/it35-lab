13156be0df177b77a266b339bfbcdff081282cad
Lorbelle143 <lorbelleganzan@gmail.com>
2025-05-14 17:53:21 +0800

    Updates

--- assets/ios.transition-legacy-Xdu-0jgv.js
+++ assets/ios.transition-legacy-Xdu-0jgv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-WY5wCrtC.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BSAO7UD_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-ZiwxR6a8.js
+++ assets/md.transition-legacy-ZiwxR6a8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-WY5wCrtC.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BSAO7UD_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
