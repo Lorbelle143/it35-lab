542a2e9ba93a8f2c66f309fb826198fcf6e82cad
Lorbelle143 <lorbelleganzan@gmail.com>
2025-05-15 18:22:19 +0800

    Updates

--- assets/swipe-back-legacy-C4i68CYy.js
+++ assets/swipe-back-legacy-C4i68CYy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BSAO7UD_.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CP3Wsplr.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
